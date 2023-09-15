f362321f8489d19dd9d3b1f0a206f7992bb0a1a9
Sarah E <seatherly.prsvr@gmail.com>
2023-09-15 12:51:52 -0500

    working commit

--- main.js
+++ main.js
@@ -97,15 +97,93 @@ function containsSpace(str) {
 }
 
 function digits(num) {
+  if(num >= 0){
+  }else if(num < 0){
+    num *= -1
+  }
+  let numStr = String(num);
+  let numArr = numStr.split('');
+  let lastArr = [];
+  for(let i=0; i < numArr.length; i++){
+    if(numArr[i] === '.'){
+      continue;
+    }else{
+      lastArr += numArr[i]
+    }
+  }
+  let numbers = lastArr.split('')
+  let answer = numbers.map(Number)
+  return answer;
 }
 
-function truncate() {
+function truncate(str) {
+  let newStr = '';
+  if(str.length >= 15){
+    for(let i = 0; i < 8; i++){
+      newStr += str[i]
+    }
+    return newStr + '...'
+  }else{
+    return str;
+  }
+  
 }
 
-function isValidPassword() {
+function isValidPassword(str) {
+  let str1 = '';
+  let str2 = '';
+  let str3 = '';
+  let str4 = '';
+  let str5 = '';
+  let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let digits = '01234567890';
+  let nonalphanumeric = "!@#$%&*()'";
+  let space = ' '
+  for(let i=0; i < str.length; i++){
+    let haslower = lowercase.indexOf(str[i]) !== -1
+    if(haslower === true){
+      str1 += str[i]
+    }
+    let hasupper = uppercase.indexOf(str[i]) !== -1
+    if(hasupper === true){
+      str2 += str[i]
+    }
+    let hasdigits = digits.indexOf(str[i]) !== -1
+    if(hasdigits === true){
+      str3 += str[i]
+    }
+    let isnonalphanumeric = nonalphanumeric.indexOf(str[i]) !== -1
+    if(isnonalphanumeric === true){
+      str4 += str[i]
+    }
+    let hasspaces = space.indexOf(str[i]) !== -1
+    if(hasspaces === true){
+      str5 += str[i]
+    }
+  }
+  if(str5.length > 0){
+    return false
+  }else if(str1.length > 0 && str2.length > 0 && str3.length > 0 && str4.length > 0){
+    return true
+  }else{
+    return false
+  }
 }
 
-function onlyPunchy() {
+function onlyPunchy(arr) {
+  let newArr = [];
+  for(let i=0; i < arr.length; i++){
+      let exclaim = arr[i].indexOf('!') !== -1
+    if(arr[i].length < 15 && exclaim === true){
+      newArr[i] = (arr[i].substring(0, '!') + '!');
+    }else if(arr[i].length < 15 && exclaim === false){
+      newArr[i] = arr[i] + '!';
+    }
+  }
+  return newArr
+
+  
 }
 
 
